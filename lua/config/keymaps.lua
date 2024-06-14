6af5c2767356c39b5911ca47b4daf5f722b05052
hpower2 <irvineafri@gmail.com>
2024-06-14 23:52:14 +0700

    change true

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,5 +3,5 @@
 -- Add any additional keymaps here
 --
 vim.keymap.set("n", "<CR>", "m`o<Esc>``")
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 vim.keymap.set("n", "<S-CR>", "m`O<Esc>``")
